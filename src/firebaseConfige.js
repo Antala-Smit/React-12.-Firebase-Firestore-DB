99f5852123e393cfa25864962ef1e97fa34b632b
Smit-Antala <6682smitantalarw@gmail.com>
2024-07-10 12:03:32 +0530

    Firebase Firestore DB

---
+++ src/firebaseConfige.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyCqedLPSl4TGuTeIeXfXN0pujuRYRUQfk0",
+    authDomain: "firestore-314f8.firebaseapp.com",
+    projectId: "firestore-314f8",
+    storageBucket: "firestore-314f8.appspot.com",
+    messagingSenderId: "421024813990",
+    appId: "1:421024813990:web:0ace652f0e3cd8d34af567",
+    measurementId: "G-J2C7CP6DHX"
+};
+
+export const app = initializeApp(firebaseConfig);
